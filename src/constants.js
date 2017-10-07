31ded8f6d11791f50269b168348b0751331e9dc3
erwanledoux <lawanledoux@gmail.com>
2017-10-07 18:45:48 +0200

    fixed camelcase

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-import camelCase from 'lodash.camelCase'
+import camelCase from 'lodash.camelcase'
 
 export const IS_ALL_DEEP_JOINS = '_is_all_deep_joins_'
 export const IS_ALL_JOINS = '_is_all_joins_'
